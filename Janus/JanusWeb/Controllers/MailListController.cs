d540174e51f93e817b7401ba7f21ec9b3cf0f032
Adrian Stangl <stangl.adrian@web.de>
2024-09-05 09:23:46 +0200

    add http client to controller

--- Janus/JanusWeb/Controllers/MailListController.cs
+++ Janus/JanusWeb/Controllers/MailListController.cs
@@ -1,12 +1,19 @@
 ﻿using JanusWeb.Services;
 using JanusWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace JanusWeb.Controllers
 {
     public class MailListController : Controller
     {
         public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public MailListController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         {
             MailList maillist = new MailList();
             EmailService emailService = new EmailService("imap.web.de", 993, true, "qwertz0014@web.de", "SSITe5tM@il");
