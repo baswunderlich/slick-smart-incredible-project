172759875dfe7d0e872ab52cd9d9112292e05102
Adrian Stangl <stangl.adrian@web.de>
2024-09-11 15:38:44 +0200

    add model for sending mail

--- Janus/JanusWeb/Business/MonsiMail.cs
+++ Janus/JanusWeb/Business/MonsiMail.cs
@@ -4,8 +4,14 @@
     {
         public string receiverDid { get; set; }
         public string senderDid { get; set; }
-        public string AESKey { get; set; }
+        public string? AESKey { get; set; }
         public string mail { get; set; }
-        public string signature { get; set; }
+        public string? signature { get; set; }
+    }
+    public class SendingMonsiMail
+    {
+        public string receiverDid { get; set; }
+        public string senderDid { get; set; }
+        public object mail { get; set; }
     }
 }
