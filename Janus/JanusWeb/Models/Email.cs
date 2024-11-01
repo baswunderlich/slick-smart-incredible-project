9d01044ba39326afa979a77b3d19f483f81aca8c
Adrian Stangl <stangl.adrian@web.de>
2024-11-01 16:06:08 +0100

    improved vc displaying

--- Janus/JanusWeb/Models/Email.cs
+++ Janus/JanusWeb/Models/Email.cs
@@ -10,6 +10,5 @@
         public string OriginalMail { get; set; }
         public string? Did { get; set; }
         public string? Signature { get; set; }
-        public string VCValid { get; set; }
     }
 }
