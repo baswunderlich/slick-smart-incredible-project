28f4327ad7caa45def4e93d3d531d7e604cd46a1
baswunderlich <bastian.wunderlich81002@gmail.com>
2024-11-06 23:15:08 +0100

    improve monsi UI and add possiblity to remove VCs

--- monsi/frontend/wailsjs/go/main/App.d.ts
+++ monsi/frontend/wailsjs/go/main/App.d.ts
@@ -4,8 +4,10 @@ import {util} from '../models';
 
 export function GetListOfDIDs():Promise<Array<util.DID>>;
 
-export function GetListOfVCs(arg1:string):Promise<Array<string>>;
+export function GetListOfVCs(arg1:string):Promise<Array<util.VC>>;
 
 export function RefreshVCs():Promise<void>;
+
+export function RemoveVC(arg1:string):Promise<void>;
 
 export function StoreVC(arg1:string,arg2:string):Promise<void>;
